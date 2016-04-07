c406b59dfdf1dec915461b3ab2e4b37e17eb2936
yaoml <yaoml@xsmdev.com>
2016-04-07 14:21:02 +0800

    reformat file update requirements

--- util/error.py
+++ util/error.py
@@ -1,8 +1,9 @@
 __author__ = 'phithon'
 
-class SpecialError(Exception):
-	def __init__(self, message):
-		self.message = message
 
-	def __str__(self):
-		return repr(self.message)
+class SpecialError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
